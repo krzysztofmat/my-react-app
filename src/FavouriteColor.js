60c9f563ffd8034396a5ae36c341e78320b77cfb
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-07-25 01:25:59 +0200

    after useState

--- src/FavouriteColor.js
+++ src/FavouriteColor.js
@@ -1,15 +1,39 @@
 import { useState } from "react";
 
 function FavouriteColor() {
+  const [color, setColor] = useState("red");
 
-    const [color, setColor] = useState("red");
+  const [brand, setBrand] = useState("Ford");
+  const [model, setModel] = useState("Mustang");
+  const [year, setYear] = useState(1964);
+  
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: 1964,
+    color: "blue"
+  });
 
-    return (
-        <>
-        <h1>My favourite color is {color}</h1>
-        <button onClick={() => setColor("blue")}>Blue</button>
-        </>
-    );
+  const changeCarColor = () => {
+    setCar((previousState) => ({...previousState, color: "red"}));
+  };
+
+  return (
+    <>
+      <h1>My favourite color is {color}</h1>
+      <button onClick={() => setColor("blue")}>Blue</button>
+      <h2>Brand {brand}</h2>
+      <p>
+        It is a {color} {model} from {year}
+      </p>
+
+      <h2>Car {car.brand}</h2>
+      <p>
+        It is a {car.color} {car.model} from {car.year}
+      </p>
+      <button type="button" onClick={changeCarColor}>Switch color of the car to blue</button>    
+    </>
+  );
 }
 
 export default FavouriteColor;
